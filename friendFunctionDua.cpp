31a31019f54ce012d43af23e373f72d307abe49a
Davinn354 <daviipad53@gmail.com>
2024-05-06 15:32:21 +0700

    Penambahan class pelajar

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -9,3 +9,12 @@ public:
     void showNilaiPelajar(pelajar &x);
 };
 
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
+};
