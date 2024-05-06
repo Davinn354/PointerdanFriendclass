2382a32dbfa4ae1aaa1213a063a898d59c2e72d6
Davinn354 <daviipad53@gmail.com>
2024-05-06 15:04:04 +0700

    Penambahan int main

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -7,6 +7,15 @@ public:
     int nim;
     void showNim()
     {
-        cout "No Induk = " << nim <, endl;
+        cout <<  "No Induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // Pointer object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
